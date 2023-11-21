f82201ad5980d308299cd22e84181eff16848e0a
Andrew Ezeani <ezeaniiandrew@gmail.com>
2023-11-21 19:01:25 +0100

    created a new pages directory

---
+++ attireflow/src/pages/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+import classes from "./landing.module.css";
+
+function Landing() {
+  return (
+    <main className={classes.home}>
+      <div className={`${classes.hero_section} header-background`}></div>
+    </main>
+  );
+}
+
+export default Landing;
