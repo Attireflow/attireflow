6cc6a9fed9759b53ae3ac9892b257ce1b628e5bc
Victor Nkire <nkirevictor77@gmail.com>
2023-11-21 23:31:35 +0100

    landing page collections section done

--- attireflow/src/pages/Landing/Landing.jsx
+++ attireflow/src/pages/Landing/Landing.jsx
@@ -8,8 +8,9 @@ function Landing() {
           Discover Your Heritage In <br /> Every Thread
         </h1>
         <p>
-          Step into a world where tradition and <br />fashion unite. Our native attire
-          collections <br /> celebrate culture with style.
+          Step into a world where tradition and <br />
+          fashion unite. Our native attire collections <br /> celebrate culture
+          with style.
         </p>
         <button className={classes.hero_btn}>Shop Now</button>
         <div className={classes.stats}>
@@ -26,6 +27,37 @@ function Landing() {
           <div>
             <h2>200K+</h2>
             <p>Monthly visits</p>
+          </div>
+        </div>
+      </div>
+      <div className={classes.collection_section}>
+        <h3>
+          Explore Our Unique Collection of Exclusive <br /> Categories
+        </h3>
+        <div className={classes.collections}>
+          <div className={`${classes.collection_product} product-background`}>
+            <p>Cord Lace Attire</p>
+            <p>200+</p>
+          </div>
+          <div className={`${classes.collection_product} product-background`}>
+            <p>Kampala Attire</p>
+            <p>250+</p>
+          </div>
+        </div>
+        <div className={classes.mid_collection}>
+          <div className={`${classes.collection_product} product-background`}>
+            <p>Ankara Attire</p>
+            <p>1000+</p>
+          </div>
+        </div>
+        <div className={classes.collections}>
+          <div className={`${classes.collection_product} product-background`}>
+            <p>Plain Cashmere Attire</p>
+            <p>500+</p>
+          </div>
+          <div className={`${classes.collection_product} product-background`}>
+            <p>Checkers Cashmere Attire</p>
+            <p>500+</p>
           </div>
         </div>
       </div>
